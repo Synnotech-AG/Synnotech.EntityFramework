c116cb40c3c7e6fa3f0a889c147fddddf2f7deb8
Thomas Trottmann <thomas.trottmann@synnotech.de>
2022-03-23 13:47:44 +0100

    Added: EfSettings and ServiceCollectionExtensions

---
+++ Code/Synnotech.EntityFramework/EfSettings.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Light.GuardClauses;
+using Light.GuardClauses.Exceptions;
+using Microsoft.Extensions.Configuration;
+
+namespace Synnotech.EntityFramework;
+
+/// <summary>
+/// Represents the default settings for Entity Framework.
+/// </summary>
+public class EfSettings
+{
+    /// <summary>
+    /// The default section name within the <see cref="IConfiguration" /> where settings are loaded from.
+    /// </summary>
+    public const string DefaultSectionName = "database";
+
+    /// <summary>
+    /// Gets or sets the connection string to the target database.
+    /// </summary>
+    public string ConnectionString { get; set; } = string.Empty;
+
+     /// <summary>
+    /// Gets or sets the value indicating whether Entity Framework statement logging is enabled. The default value is false.
+    /// </summary>
+    public bool IsLoggingEnabled { get; set; }
+
+    /// <summary>
+    /// Loads the <see cref="EfSettings" /> from configuration.
+    /// </summary>
+    /// <param name="configuration">The configuration instance where the settings are loaded from.</param>
+    /// <param name="sectionName">The name of the section that represents the EF settings.</param>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="configuration" /> or <paramref name="sectionName" /> is null.</exception>
+    /// <exception cref="ArgumentException">Thrown when <paramref name="sectionName" /> is an empty string or contains only whitespace.</exception>
+    /// <exception cref="InvalidConfigurationException">Thrown when the settings could not be loaded (most likely because the section is not present in the configuration).</exception>
+    public static EfSettings FromConfiguration(IConfiguration configuration, string sectionName = DefaultSectionName) =>
+        FromConfiguration<EfSettings>(configuration, sectionName);
+
+    /// <summary>
+    /// Loads the EF settings from configuration.
+    /// </summary>
+    /// <typeparam name="T">The type of EF settings that will be used to load the settings.</typeparam>
+    /// <param name="configuration">The configuration instance where the settings are loaded from.</param>
+    /// <param name="sectionName">The name of the section that represents the EF settings.</param>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="configuration" /> or <paramref name="sectionName" /> is null.</exception>
+    /// <exception cref="ArgumentException">Thrown when <paramref name="sectionName" /> is an empty string or contains only whitespace.</exception>
+    /// <exception cref="InvalidConfigurationException">Thrown when the settings could not be loaded (most likely because the section is not present in the configuration).</exception>
+    public static T FromConfiguration<T>(IConfiguration configuration, string sectionName = DefaultSectionName)
+    {
+        configuration.MustNotBeNull(nameof(configuration));
+        sectionName.MustNotBeNullOrWhiteSpace(nameof(sectionName));
+        return configuration.GetSection(sectionName)
+                            .Get<T?>() ?? throw new InvalidConfigurationException($"EF settings could not be retrieved from configuration section \"{sectionName}\".");
+    }
+}
---
+++ Code/Synnotech.EntityFramework/ServiceCollectionExtensions.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Data.Entity;
+using Light.GuardClauses;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Synnotech.Core.DependencyInjection;
+
+namespace Synnotech.EntityFramework;
+
+/// <summary>
+/// Provides extension methods for registering Entity Framework with the DI container.
+/// </summary>
+public static class ServiceCollectionExtensions
+{
+    /// <summary>
+    /// Registers your DB context with the DI container.
+    /// </summary>
+    /// <typeparam name="TDbContext">Your subtype that derives from <see cref="DbContext" />.</typeparam>
+    /// <param name="services">The collection that holds all registrations for the DI container.</param>
+    /// <param name="dbContextLifetime">
+    /// The lifetime of the DB context (optional). The default value is <see cref="ServiceLifetime.Transient" />.
+    /// </param>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="services" /> is null.</exception>
+    public static IServiceCollection AddDbContext<TDbContext>(this IServiceCollection services,
+                                                              ServiceLifetime dbContextLifetime = ServiceLifetime.Transient)
+        where TDbContext : DbContext
+    {
+        services.MustNotBeNull(nameof(services))
+                .Add(new ServiceDescriptor(typeof(TDbContext), typeof(TDbContext), dbContextLifetime));
+        return services;
+    }
+
+    /// <summary>
+    /// Registers your Entity Framework settings with the DI container
+    /// </summary>
+    /// <param name="services">The collection that holds all registrations for the DI container.</param>
+    /// <param name="configurationSectionName">The configurationSectionName is used to get the correct section from your settings file. The default value is "database"</param>
+    public static IServiceCollection AddEfSettings(this IServiceCollection services,
+                                                   string configurationSectionName = EfSettings.DefaultSectionName)
+    {
+        services.AddSingleton(container => EfSettings.FromConfiguration(container.GetRequiredService<IConfiguration>(), configurationSectionName));
+        return services;
+    }
+
+    /// <summary>
+    ///  Enables Logging in case you set IsLoggingEnabled in your EfSettings to true
+    /// </summary>
+    /// <param name="context">The implementation of DbContext</param>
+    /// <param name="logger">The logger you are using.</param>
+    /// <param name="efSettings">The Entity Framework you set. To enable logging, you must set set IsLoggingEnabled in your EfSettings to true</param>
+    /// <typeparam name="TDbContext">Your subtype that derives from <see cref="DbContext" />.</typeparam>
+    public static TDbContext EnableLoggingIfNecessary<TDbContext>(this TDbContext context, ILogger logger, EfSettings efSettings)
+        where TDbContext : DbContext
+    {
+        context.MustNotBeNull();
+        efSettings.MustNotBeNull();
+        logger.MustNotBeNull();
+        if (efSettings.IsLoggingEnabled)
+            context.Database.Log = message => logger.LogInformation(message);
+
+        return context;
+    }
+
+
+    /// <summary>
+    /// Registers a session with the DI container and a Func&lt;TAbstract&gt; factory delegate that can be used to
+    /// resolve the session.
+    /// </summary>
+    /// <typeparam name="TAbstraction">The session abstraction.</typeparam>
+    /// <typeparam name="TImplementation">The session implementation.</typeparam>
+    /// <param name="services">The collection that holds all registrations for the DI container.</param>
+    /// <param name="lifetime">
+    /// The lifetime of the session (optional). The default value is <see cref="ServiceLifetime.Transient" />,
+    /// i.e. callers must dispose the session by themselves.
+    /// </param>
+    /// <param name="registerFunc">
+    /// The value indicating whether the factory delegate should be registered (optional). The default value
+    /// is true. You can set this value to false if your DI container supports this automatically, e.g.
+    /// LightInject supports Function Factories.
+    /// </param>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="services" /> is null.</exception>
+    public static IServiceCollection AddSession<TAbstraction, TImplementation>(this IServiceCollection services,
+                                                                               ServiceLifetime lifetime = ServiceLifetime.Transient,
+                                                                               bool? registerFunc = null)
+        where TAbstraction : class, IDisposable
+        where TImplementation : class, TAbstraction
+    {
+        services.MustNotBeNull();
+        services.Add(new ServiceDescriptor(typeof(TAbstraction), typeof(TImplementation), lifetime));
+        if (ContainerSettingsContext.Settings.CheckIfFactoryDelegateShouldBeRegistered(registerFunc))
+            services.AddSingleton<Func<TAbstraction>>(container => container.GetRequiredService<TAbstraction>);
+        return services;
+    }
+}
