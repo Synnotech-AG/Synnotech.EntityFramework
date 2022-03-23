b076c4e6839016c393ff438cb3b4527589ca9f12
Thomas Trottmann <thomas.trottmann@synnotech.de>
2022-03-23 10:42:54 +0100

    Added: TransactionalSession

---
+++ Code/Synnotech.EntityFramework/EfTransaction.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Data.Entity;
+using Light.GuardClauses;
+using Synnotech.DatabaseAbstractions;
+
+namespace Synnotech.EntityFramework;
+
+/// <summary>
+/// Represents an adapter for the Entity Framework Core <see cref="DbContextTransaction" /> that
+/// implements <see cref="ITransaction" />. The transaction will be implicitly rolled back
+/// when commit was not called and the transaction was disposed.
+/// </summary>
+public sealed class EfTransaction : ITransaction
+{
+    /// <summary>
+    /// Initializes a new instance of <see cref="EfTransaction" />.
+    /// </summary>
+    /// <param name="transaction">The EF transaction that will be wrapped by this instance.</param>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="transaction" /> is null.</exception>
+    public EfTransaction(DbContextTransaction transaction) =>
+        Transaction = transaction.MustNotBeNull(nameof(transaction));
+
+
+    private DbContextTransaction Transaction { get; }
+
+    /// <summary>
+    /// Disposes the underlying transaction. It will also be rolled back if <see cref="Commit" />
+    /// was not called.
+    /// </summary>
+    public void Dispose() => Transaction.Dispose();
+
+    /// <summary>
+    /// Commits the underlying EF transaction.
+    /// </summary>
+    public void Commit() => Transaction.Commit();
+}
---
+++ Code/Synnotech.EntityFramework/ITransactionalSession.cs
@@ -0,0 +1,10 @@
+﻿using Synnotech.DatabaseAbstractions;
+
+namespace Synnotech.EntityFramework;
+
+/// <summary>
+/// Represents an asynchronous session to a database that is able to create individual transactions.
+/// Use this interface if you are using EF in the background instead of the one from <see cref="DatabaseAbstractions.ITransactionalSession" />,
+/// because you need an additional SaveChanges method.
+/// </summary>
+public interface ITransactionalSession : DatabaseAbstractions.ITransactionalSession, ISession { }
