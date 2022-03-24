0560a184298abc585a7e42f61d39391f4be4f4da
Thomas Trottmann <thomas.trottmann@synnotech.de>
2022-03-24 11:43:10 +0100

    Added: AsyncReadOnlySession Tests

---
+++ Code/Synnotech.EntityFramework.Tests/AsyncReadOnlySessionIntegrationTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace Synnotech.EntityFramework.Tests;
+
+public sealed class AsyncReadOnlySessionIntegrationTests : DbIntegrationTest
+{
+    [SkippableFact]
+    public async Task ReadDataAsync()
+    {
+        var connectionString = GetConnectionStringOrSkip();
+
+        var context = new TestContext(connectionString);
+        var expectedContacts = new Contact[]
+        {
+            new () { Id = 1, LastName = "Doe", FirstName = "John", Age = 43 },
+            new () { Id = 2, LastName = "Dos Santos", FirstName = "Maria", Age = 24 },
+        };
+
+        await using var session = new EfGetContactAsyncSession(context);
+        var contacts = await session.GetContactsAsync();
+
+        contacts.Should().BeEquivalentTo(expectedContacts);
+    }
+
+    private interface IGetContactSessionAsync
+    {
+        Task<List<Contact>> GetContactsAsync();
+    }
+
+    private sealed class EfGetContactAsyncSession : AsyncReadOnlySession<TestContext>, IGetContactSessionAsync
+    {
+        public EfGetContactAsyncSession(TestContext context) : base(context) { }
+        public Task<List<Contact>> GetContactsAsync() =>
+            Context.Contacts.ToListAsync();
+    }
+}
---
+++ Code/Synnotech.EntityFramework.Tests/AsyncReadOnlySessionTests.cs
@@ -0,0 +1,13 @@
+﻿using FluentAssertions;
+using Synnotech.DatabaseAbstractions;
+using Xunit;
+
+namespace Synnotech.EntityFramework.Tests;
+
+public static class AsyncReadOnlySessionTests
+{
+    [Fact]
+    public static void MustImplementIAsyncReadOnlySession() =>
+        typeof(AsyncReadOnlySession<>).Should().Implement<IAsyncReadOnlySession>();
+
+}
