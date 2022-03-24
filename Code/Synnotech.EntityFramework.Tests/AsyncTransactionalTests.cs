4f0413f4566af5d760887c8d8a54a0d63a53ed5d
Thomas Trottmann <thomas.trottmann@synnotech.de>
2022-03-24 13:49:04 +0100

    Added: AsyncTransactionalTest and TransactionalTests

---
+++ Code/Synnotech.EntityFramework.Tests/AsyncTransactionalTests.cs
@@ -0,0 +1,15 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Synnotech.EntityFramework.Tests;
+
+public static class AsyncTransactionalTests
+{
+    [Fact]
+    public static void MustImplementIAsyncTransactionalSession() =>
+        typeof(AsyncTransactionalSession<>).Should().Implement<IAsyncTransactionalSession>();
+
+    [Fact]
+    public static void MustDeriveFromAsyncSession() =>
+        typeof(AsyncTransactionalSession<>).Should().BeDerivedFrom(typeof(AsyncSession<>));
+}
---
+++ Code/Synnotech.EntityFramework.Tests/TransactionalTests.cs
@@ -0,0 +1,15 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Synnotech.EntityFramework.Tests;
+
+public static class TransactionalTests
+{
+    [Fact]
+    public static void MustImplementITransactionalSession() =>
+        typeof(TransactionalSession<>).Should().Implement<ITransactionalSession>();
+
+    [Fact]
+    public static void MustDeriveFromSession() =>
+        typeof(TransactionalSession<>).Should().BeDerivedFrom(typeof(Session<>));
+}
