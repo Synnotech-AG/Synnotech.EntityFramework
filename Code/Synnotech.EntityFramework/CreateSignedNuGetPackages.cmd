fe04383e40a490b4ad7e708d10641de3f824527a
Kenny Pflug <kenny.pflug@live.de>
2022-03-24 16:11:43 +0100

    added script to create NuGet packages

---
+++ Code/Synnotech.EntityFramework/CreateSignedNuGetPackages.cmd
@@ -0,0 +1,1 @@
+dotnet pack Synnotech.EntityFramework.csproj --configuration Release /p:SignAssembly=true /p:AssemblyOriginatorKeyFile=bin\Synnotech.EntityFramework.snk /p:ContinuousIntegrationBuild=true
